04b81dab3446fdeee327f34b5be1eccc123a8deb
celfonica <servicioscelfonica@gmail.com>
2020-08-05 03:52:32 -0700

    adding vue loader + first mayn component Home

---
+++ app/javascript/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+
+<h1>this is home template</h1>
+
+</template>
+
+
+
+<script>
+
+import Vuetify from 'vuetify'
+import Vue from 'vue/dist/vue.esm'
+
+export default {
+  name: 'home'
+}
+
+</script>
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,10 @@
 const { environment } = require('@rails/webpacker')
 const coffee =  require('./loaders/coffee')
+const vue = require('./loaders/vue')
+
 
 environment.loaders.prepend('coffee', coffee)
+environment.loaders.prepend('vue', vue)
+
+
 module.exports = environment
