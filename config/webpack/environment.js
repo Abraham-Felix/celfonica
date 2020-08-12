ba32b34ef2891b57a8b945b5af265437009c6b8c
celfonica <servicioscelfonica@gmail.com>
2020-08-12 02:03:43 -0700

    css loader installation

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,10 @@
 const { environment } = require('@rails/webpacker')
 const coffee =  require('./loaders/coffee')
 const vue = require('./loaders/vue')
-
+const css = require('./loaders/css')
 
 environment.loaders.prepend('coffee', coffee)
 environment.loaders.prepend('vue', vue)
-
+environment.loaders.prepend('css', css)
 
 module.exports = environment
